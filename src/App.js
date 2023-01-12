64b57a32fa308a84346573084929075b93d531ce
Tyler Hoffman <tjhoffmn@gmail.com>
2023-01-12 13:10:52 -0800

    add working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './css/App.css';
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
+import View from './Pages/ViewPage';
 import AddPage from './Pages/AddPage';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/add/:imageId" element={<AddPage />} />
+        <Route path="/view/:id" element={<View />} />
       </Routes>
     </div>
   );
