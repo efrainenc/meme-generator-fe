2688628d03fe35358acbe707dbbf4d97d3eb57dc
unknown <efraine387@gmail.com>
2023-01-12 13:46:07 -0500

    fetch in place

--- src/components/Memes.jsx
+++ src/components/Memes.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useState, useEffect } from "react"
 
 const Memes = () => {
+
+  const [memeState, setMemeState] = useState({})
+
+  const memeURL = `https://meme-generator.herokuapp.com/`
+
+  const getMemes= async()=>
+  {
+    try
+    {
+      const res= await fetch(memeURL)
+      const allMemes= await res.json()
+      setMemeState(allMemes)
+    }catch(err)
+    {
+      console.log(err)
+    }
+  }
+
+
+  useEffect(()=>{getMemes();}, [])
   return (
     <div>Memes</div>
   )
