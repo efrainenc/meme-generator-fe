d3373f5bb715b744ae067dbe3019d34857c61718
unknown <efraine387@gmail.com>
2023-01-12 12:13:22 -0500

    Meme component made

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './css/App.css';
+import React from 'react'
+import Memes from './components/Memes';
 
 function App() {
   return (
     <div className="App">
       <h1> MEME GENERATOR</h1>
+      <Memes />
     </div>
   );
 }
