9768a13bd5048a0d79629d4f86ec114ce41fece3
unknown <efraine387@gmail.com>
2023-01-12 15:45:04 -0500

    memes landing page

--- src/components/Memes.jsx
+++ src/components/Memes.jsx
@@ -7,9 +7,9 @@ const Memes = () => {
   // setting fetched memes to state to display
   const [memeState, setMemeState] = useState({})
 
-  // url to memes db
-  const memeURL = `https://hack-a-meme.herokuapp.com/meme`
-
+  // url to memes db for show
+  const memeURL = `https://hack-a-meme.herokuapp.com/meme/`
+  
   // fetch to get memes
   const getMemes= async()=>
   {
@@ -24,22 +24,44 @@ const Memes = () => {
     }
   }
 
+  // Loaded Post function
+  const loaded = () =>
+  {
+    // JSX for creating a new post when post is loaded
+    return (
+      <section className='meme-list'>
+        {memeState?.map((memeMap, memeMapKey) =>
+          {
+            return(
+            <div key={memeMapKey} className='memes'>
+              <img src={memeMap.image} />
+            </div>
+          )
+          })
+        }
+      </section>
+    )
+  };
+
+  // / JSX for creating a new post when post is loading
+  const loading = () => (
+    <section className="loading">
+      <h1>
+        Loading...
+        <span>
+          <img
+            className="spinner"
+            src="https://freesvg.org/img/1544764567.png"
+          />{" "}
+        </span>
+      </h1>
+    </section>
+  );
+
   // gets all memes on page load
   useEffect(()=>{getMemes();}, [])
 
-  return (
-    <>
-      {/* {memeState?.map((memeMap, memeMapKey) =>
-        {
-          return(
-          <div key={memeMapKey} className='memes'>
-            <img src={memeMap.image} />
-          </div>
-        )
-        })
-      } */}
-    </>
-  )
+  return (memeState ? loaded() : loading())
 }
 
 export default Memes
