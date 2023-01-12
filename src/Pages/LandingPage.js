921737b0c7f5db6f9d01ce874dfbf5fd9a241029
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-12 15:21:21 -0600

    adjusted text for half of the meme images

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -4,57 +4,6 @@ const LandingPage = () => {
   return (
     <div>
       <h1>MEME GENERATOR</h1>
-      <img width="200" src="https://i.imgflip.com/3pdf2w.png" alt= "Bernie Sanders" />
-      <img width="200" src="https://imgflip.com/s/meme/Two-Buttons.jpg" alt="Two Buttons" />
-      <img width="200" src="https://imgflip.com/s/meme/Running-Away-Balloon.jpg" alt="Running Away Balloon" />
-      <img width="200" src="https://imgflip.com/s/meme/Buff-Doge-vs-Cheems.png" alt="Buff Doge vs. Cheems" />
-      <img width="200" src="https://imgflip.com/s/meme/Grus-Plan.jpg" alt="Gru's Plan" />
-      <img width="200" src="https://imgflip.com/s/meme/Expanding-Brain.jpg" alt="Expanding Brain" />
-      <img width="200" src="https://imgflip.com/s/meme/Sad-Pablo-Escobar.jpg" alt="Sad Pablo Escobar" />
-      <img width="200" src="https://imgflip.com/s/meme/X-X-Everywhere.jpg" alt="X, X Everywhere" />
-      <img width="200" src="https://imgflip.com/s/meme/Tuxedo-Winnie-The-Pooh.png" alt="Tuxedo Winnie the Pooh" />
-      <img width="200" src="https://imgflip.com/s/meme/Hide-the-Pain-Harold.jpg" alt="Hide the Pain Harold" />
-      <img width="200" src="https://imgflip.com/s/meme/One-Does-Not-Simply.jpg" alt="One Does Not Simply" />
-      <img width="200" src="https://imgflip.com/s/meme/Blank-Nut-Button.jpg" alt="Blank Nut Button" />
-      <img width="200" src="https://imgflip.com/s/meme/Roll-Safe-Think-About-It.jpg" alt="Roll Safe Thinking About It" />
-      <img width="200" src="https://imgflip.com/s/meme/Mocking-Spongebob.jpg" alt="Mocking Spongebob" />
-      <img width="200" src="https://imgflip.com/s/meme/Monkey-Puppet.jpg" alt="Monkey Puppet" />
-      <img width="200" src="https://imgflip.com/s/meme/Left-Exit-12-Off-Ramp.jpg" alt="Left Exit 12 Off Ramp" />
-      <img width="200" src="https://imgflip.com/s/meme/Drake-Hotline-Bling.jpg" alt="Drake Hotline Bling" />
-      <img width="200" src="https://imgflip.com/s/meme/Distracted-Boyfriend.jpg" alt="Distracted Boyfriend" />
-      <img width="200" src="https://imgflip.com/s/meme/Disaster-Girl.jpg" alt="Disaster Girl" />
-      <img width="200" src="https://imgflip.com/s/meme/Batman-Slapping-Robin.jpg" alt="Batman Slapping Robin" />
-      <img width="200" src="https://imgflip.com/s/meme/Bike-Fall.jpg" alt="Bike Fall" />
-      <img width="200" src="https://imgflip.com/s/meme/Ancient-Aliens.jpg" alt="Ancient Aliens" />
-      <img width="200" src="https://imgflip.com/s/meme/This-Is-Fine.jpg" alt="This is Fine" />
-      <img width="200" src="https://i.imgflip.com/2reqtg.png" alt="Flex Tape" />
-      <img width="200" src="https://imgflip.com/s/meme/Evil-Kermit.jpg" alt="Evil Kermit" />
-      <img width="200" src="https://i.imgflip.com/46hhvr.jpg" alt="Mother Ignoring Kid Drowning in a Pool" />
-      <img width="200" src="https://imgflip.com/s/meme/Leonardo-Dicaprio-Cheers.jpg" alt="Leonardo Decaprio Cheers" />
-      <img width="200" src="https://imgflip.com/s/meme/Spongebob-Ight-Imma-Head-Out.jpg" alt="Spongebob Ight Imma Head Out" />
-      <img width="200" src="https://imgflip.com/s/meme/Futurama-Fry.jpg" alt="Futurama Fry" />
-      <img width="200" src="https://i.imgflip.com/u0pf0.jpg" alt="Disappointed Black Guy" />
-      <img width="200" src="https://imgflip.com/s/meme/Success-Kid.jpg" alt="Success Kid" />
-      <img width="200" src="https://i.imgflip.com/1itoun.jpg" alt="Charlie Conspiracy" />
-      <img width="200" src="https://i.imgflip.com/1c81c1.jpg" alt="Math Lady" />
-      <img width="200" src="https://i.imgflip.com/34vt4i.jpg" alt="Brain Before Sleep" />
-
-      <img width="200" src="https://i.imgflip.com/2ji8hx.jpg" alt="The Office Congratulations" />
-
-      <img width="200" src="https://imgflip.com/s/meme/That-Would-Be-Great.jpg" alt="That Would Be Great" />
-
-      <img width="200" src="https://i.imgflip.com/bwu6w.jpg" alt="C'mon Do Something" />
-
-      <img width="200" src="https://imgflip.com/s/meme/Imagination-Spongebob.jpg" alt="Imagination Spongebob" />
-
-      <img width="200" src="https://i.imgflip.com/1hhv9m.jpg" alt="Obama Medal" />
-
-      <img width="200" src="https://imgflip.com/s/meme/First-World-Problems.jpg" alt="First World Problems" />
-
-      <img width="200" src="https://i.imgflip.com/39osmx.png" alt="Kombucha Girl" />
-
-
-      
 
     </div>
   )
