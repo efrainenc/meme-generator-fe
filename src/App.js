f4758d4e7834a8c65c84d3ed59817609e65f8b8f
EmilyJarecki <jareckiemily@yahoo.com>
2023-01-12 12:05:04 -0600

    created image drop down. Input field for top and bottom line.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './css/App.css';
+import Choices from './Images/Choices';
 
 function App() {
   return (
     <div className="App">
       <h1> MEME GENERATOR</h1>
+      <Choices />
     </div>
   );
 }
